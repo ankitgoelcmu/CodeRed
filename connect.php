1c63cbee1cc89600917aa98e65f4403a5f5b0de0
Ankit Goel <ankitgoelcmu@users.noreply.github.com>
2016-03-17 15:21:32 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@
 	$user="agoel001";
 	$pass="Sumologic12!";
 	$dbname="dbseriver";
-//Data Source=tcp:dbseriver.database.windows.net,1433;Initial Catalog=ECommerce;User ID=agoel001@dbseriver;Password=Sumologic12!
+//Source=tcp:dbseriver.database.windows.net,1433;Initial Catalog=ECommerce;User ID=agoel001@dbseriver;Password=Sumologic12!
 	
 	$con=mysqli_connect($host, $user, $pass, $dbname);
 	
